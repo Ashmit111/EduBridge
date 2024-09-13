18d033014b8563fc150378da389de521b1d7e8e9
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-09-13 22:26:30 +0530

    solved error

--- Frontend/tailwind.config.cjs
+++ Frontend/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-const flowbite = require("flowbite-react/tailwind");
 module.exports = {
-  content: ["./src/**/*.{js,jsx}",flowbite.content(),],
+  content: ["./src/**/*.{js,jsx}",
+   ],
   mode: "jit",
   theme: {
     extend: {
@@ -24,5 +24,5 @@ module.exports = {
       },
     },
   },
-  plugins: [flowbite.plugin(),],
+  plugins: [],
 };

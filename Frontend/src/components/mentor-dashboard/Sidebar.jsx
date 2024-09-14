1cba85fff4e74d9786c8a38b2b972972643c6eba
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-09-14 15:55:36 +0530

    resolved errors

--- Frontend/src/components/mentor-dashboard/Sidebar.jsx
+++ Frontend/src/components/mentor-dashboard/Sidebar.jsx
@@ -1,35 +1,21 @@
 import React from 'react'
 import { FaUser, FaUserTie, FaCalendar, FaQuestionCircle, FaSignOutAlt,FaMeetup } from 'react-icons/fa';
 import { IoIosAddCircleOutline } from "react-icons/io";
-import { useNavigate } from 'react-router-dom';
-
-
-
 
 const SideBar = () => {
-    const navigate = useNavigate()
-    const ChooseProfile = () => {
-        navigate(`/mentor-dashboard/profile`);
-    }
-    const AddPost = () => {
-        navigate(`/mentor-dashboard/add-post`);
-    }
-    const MyBlogs = () => {
-        navigate(`/mentor-dashboard/myblog`);
-    }
   return (
         <div className="w-64 bg-[#0a0e1f] p-6">
         <h2 className="text-2xl font-bold mb-6">Dashboard</h2>
         <ul className="space-y-4">
-          <li className="flex items-center space-x-2 cursor-pointer hover:text-blue-400 transition-colors" onClick={ChooseProfile}>
+          <li className="flex items-center space-x-2 cursor-pointer hover:text-blue-400 transition-colors">
             <FaUser />
             <span>Profile</span>
           </li>
-          <li className="flex items-center space-x-2 cursor-pointer hover:text-blue-400 transition-colors" onClick={AddPost}>
+          <li className="flex items-center space-x-2 cursor-pointer hover:text-blue-400 transition-colors">
           <IoIosAddCircleOutline />
             <span>Add Blog</span>
           </li>
-          <li className="flex items-center space-x-2 cursor-pointer hover:text-blue-400 transition-colors"onClick={MyBlogs}>
+          <li className="flex items-center space-x-2 cursor-pointer hover:text-blue-400 transition-colors">
             <FaUserTie />
             <span>My Blogs</span>
           </li>

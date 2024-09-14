9ac86fdb724c9952b93f924ed80f3cefcc092fb0
Ashmit111 <ashmitsingh862@gmail.com>
2024-09-14 10:03:01 +0530

    Dashboard Added

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./components/Home/Home";
-import Login from "./components/second/login";
+import Login from "./components/Login/login"
 import Layout from "./Layout";
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom'
+import Dashboard from "./components/Dashboard/Dashboard";
 
 
 import App from "./App";
 import "./index.css";
-import Layout from "../layout";
 import AddPost from "./components/AddPosts/AddPost";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
+    <Route>
       <Route path="home" element={<Home/>}/>
       <Route path="login" element={<Login/>}/>
       <Route path="add-post" element={<AddPost/>}/>
+      <Route path="dashboard" element={<Dashboard/>}/>
     </Route>
   )
   
@@ -24,7 +25,7 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
 
